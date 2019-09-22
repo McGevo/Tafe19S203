3703d05b4b5a983f22ddb649a8237372c70cfbe9
Sean Daly <smdaly88@gmail.com>
2019-09-22 17:05:47 +0930

    Commit feat_PersonalInfo

---
+++ feat_PersonalInfo/Start-Finance-master/InstaRichie/Models/PersonalInfo.cs
@@ -0,0 +1,33 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    public class PersonalInfo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+   
+        public string FirstName { get; set; }
+
+      
+        public string LastName { get; set; }
+
+        
+        public string Email { get; set; }
+
+        
+        public string Phone { get; set; }
+
+     
+        public string DOB { get; set; }
+
+       
+        public string Gender { get; set; }
+    }
+}
