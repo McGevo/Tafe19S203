719f5efc90493cc7e2237f1348412873745a6fbd
Steven <steven.frankling@student.tafesa.edu.au>
2019-10-28 14:59:38 +1030

    Relocating folders

---
+++ Start-Finance-master/InstaRichie/Models/ShoppingItem.cs
@@ -0,0 +1,27 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    class ShoppingItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ShoppingItemID { get; set; }
+
+        [NotNull]
+        public string ShopName { get; set; }
+
+        [Unique, NotNull]
+        public string NameOfItem { get; set; }
+
+        [NotNull]
+        public DateTime ShoppingDate { get; set; }
+
+        [NotNull]
+        public double PriceQuoted { get; set; }
+    }
+}
